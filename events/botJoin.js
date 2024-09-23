cf3546318fc19984f4ed06cbfc3662d623d1b171
daneeskripter <imdanee@seznam.cz>
2024-09-23 19:07:11 +0300

    add users to db after bot join server

--- events/botJoin.js
+++ events/botJoin.js
@@ -4,14 +4,16 @@ const User = require("../models/User")
 module.exports = {
 	name: Events.GuildCreate,
 	once: false,
-	execute(guild) {
-        guild.members.cache.forEach(member => {
+	async execute(guild) {
+       const members = await guild.members.fetch()
+        members.forEach(async member => {
+            if (member.user.bot) return;
+            if (await User.findOne({where: {discordId: member.id, guildId: guild.id}})) return;
             User.create({
                 discordId: member.id,
                 guildId: guild.id,
                 coins: 0
             })
         })
-
     },
 };
